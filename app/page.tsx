325ebfd2c55cc2747f5353959e626e530288c0c7
Anirudh-Rb26 <anirudhjayakumar.business@gmail.com>
2025-11-14 00:46:58 +0530

    Fix: Handles edge case when all reviews are filtered out.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import { useMemo, useState } from "react";
-import { mockReviews } from "@/lib/mockdata";
+import { Button } from "@/components/ui/button";
+import { mockReviews, Review } from "@/lib/mockdata";
 import { useReviews } from "@/contexts/ReviewContext";
 import { ReviewTable } from "@/components/review-table";
 import { ReviewFilters } from "@/components/review-filters";
 import { BulkActionsBar } from "@/components/bulk-actionbar";
 import { BulkReplyModal } from "@/components/bulk-reply-modal";
-import { Button } from "@/components/ui/button";
 
 export default function Home() {
   const { uploadedReviews } = useReviews();
@@ -19,6 +19,7 @@ export default function Home() {
   const [showBulkReplyModal, setShowReplyModal] = useState(false);
   const [selectedLocation, setSelectedLocation] = useState("All Locations");
   const [selectedSentiment, setSelectedSentiment] = useState("All Sentiments");
+  const [baseReview, setBaseReview] = useState<Review[]>();
 
   const filteredReviews = useMemo(() => {
     const reviews = uploadedReviews.length > 0 ? uploadedReviews : (showMockData ? mockReviews : []);
@@ -40,7 +41,8 @@ export default function Home() {
     })
   }, [searchQuery, selectedLocation, selectedSentiment, dateFrom, dateTo, uploadedReviews, showMockData])
 
-  const locations = ["All Locations", ...new Set(uploadedReviews.map(review => review.location))];
+  const activeReviews = uploadedReviews.length > 0 ? uploadedReviews : showMockData ? mockReviews : [];
+  const locations = ["All Locations", ...new Set(activeReviews.map(review => review.location))];
   const selectedReviews = (uploadedReviews.length > 0 ? uploadedReviews : mockReviews)
     .filter((review) => selectedIds.includes(review.id));
 
@@ -51,53 +53,73 @@ export default function Home() {
         <h4 className="text-md mt-1">All Customer Reviews</h4>
       </div>
       <div className="my-3"></div>
-      {filteredReviews.length === 0 ? (
-        <div className="flex flex-col gap-5 mt-10 items-center justify-center">
-          <div className="flex flex-col items-center justify-center">
-            <h1 className="text-3xl">No Reviews Available!</h1>
-            <p className="text-md mt-1">Do you want to use Mockdata?</p>
+      {
+        !baseReview || baseReview?.length === 0 ? (
+          <div className="flex flex-col gap-5 mt-10 items-center justify-center">
+            <div className="flex flex-col items-center justify-center">
+              <h1 className="text-3xl">No Reviews Available!</h1>
+              <p className="text-md mt-1">Upload your JSON or use MockData</p>
+            </div>
+            <Button
+              variant="outline"
+              className="text-black border-none hover:bg-[#181818] hover:text-white transition-colors duration-300"
+              onClick={() => {
+                const basereview = uploadedReviews.length > 0 ? uploadedReviews :
+                  (showMockData ? mockReviews : []);
+                setBaseReview(basereview)
+                setShowMockData(true)
+              }}>
+              Use Mockdata?
+            </Button>
           </div>
-          <Button
-            variant="outline"
-            className="text-black border-none hover:bg-[#181818] hover:text-white transition-colors duration-300"
-            onClick={() => { setShowMockData(true) }}>
-            Use Mockdata?
-          </Button>
-        </div>
-      ) : (<div>
-        <ReviewFilters
-          searchQuery={searchQuery}
-          onSearchChange={setSearchQuery}
-          selectedLocation={selectedLocation}
-          onLocationChange={setSelectedLocation}
-          selectedSentiment={selectedSentiment}
-          onSentimentChange={setSelectedSentiment}
-          dateFrom={dateFrom}
-          onDateFromChange={setDateFrom}
-          dateTo={dateTo}
-          onDateToChange={setDateTo}
-          activeLocation={locations}
-        />
+        ) : (
+          <div>
+            <ReviewFilters
+              searchQuery={searchQuery}
+              onSearchChange={setSearchQuery}
+              selectedLocation={selectedLocation}
+              onLocationChange={setSelectedLocation}
+              selectedSentiment={selectedSentiment}
+              onSentimentChange={setSelectedSentiment}
+              dateFrom={dateFrom}
+              onDateFromChange={setDateFrom}
+              dateTo={dateTo}
+              onDateToChange={setDateTo}
+              activeLocation={locations}
+            />
 
-        <div className="my-3"></div>
+            <div className="my-3"></div>
 
-        <ReviewTable
-          reviews={filteredReviews}
-          selectedIds={selectedIds}
-          onSelectionChange={setSelectedIds}
-        />
-        <BulkActionsBar
-          selectedCount={selectedIds.length}
-          onSuggestReplies={() => setShowReplyModal(true)}
-          onCopySelected={() => { }}
-          onClearSelection={() => setSelectedIds([])}
-        />
-        <BulkReplyModal
-          open={showBulkReplyModal}
-          onOpenChange={setShowReplyModal}
-          reviews={selectedReviews}
-        />
-      </div>)
+            {
+              filteredReviews.length === 0 ? (
+                <div className="flex flex-col gap-5 mt-10 items-center justify-center">
+                  <div className="flex flex-col items-center justify-center">
+                    <h1 className="text-3xl">No Reviews Available!</h1>
+                    <p className="text-md mt-1">Try adjusting your filters</p>
+                  </div>
+                </div>
+              ) : (
+                <ReviewTable
+                  reviews={filteredReviews}
+                  selectedIds={selectedIds}
+                  onSelectionChange={setSelectedIds}
+                />
+              )
+            }
+
+            <BulkActionsBar
+              selectedCount={selectedIds.length}
+              onSuggestReplies={() => setShowReplyModal(true)}
+              onCopySelected={() => { }}
+              onClearSelection={() => setSelectedIds([])}
+            />
+            <BulkReplyModal
+              open={showBulkReplyModal}
+              onOpenChange={setShowReplyModal}
+              reviews={selectedReviews}
+            />
+          </div>
+        )
       }
     </div >
   );
